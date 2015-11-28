36777adbe8e6ec790d31faa37b331b70c64e1ff2
Will Howell <will.s.howell@gmail.com>
2015-11-28 00:13:37 -0500

    Actual comparing of old favorites in the database with new ones from soundcloud. Saves them in the database. Updates the target timestamp

--- worker.py
+++ worker.py
@@ -1,39 +1,49 @@
 import soundcloud_helper as sch, models
 import time, datetime
 
+def update_target_in_db(target, amount=5):
+	print("Updating: {}, id={}".
+		format(target.permalink, target.sc_id))
+	print("Last updated at: {}".format(target.updated_at))
+
+	# Figure out how many new likes have
+	# occurred since the last check
+	prev_faves = []
+	for track in target.get_tracks():
+		prev_faves.append(track.sc_id)
+	new_faves = sch.get_favorites(target.sc_id, amount)
+
+	# If new songs liked:
+	#prev = [6, 5, 4, 3, 2, 1, 0]
+	#new  = [8, 7, 6, 4, 3]
+	diff = [item for item in new_faves if item not in prev_faves]
+	diff.reverse()
+	if diff:
+		print("Need to add {}.".format(diff))
+	else:
+		print("No new tracks to add.")
+		return
+
+	# Just take the new ones and save them in order
+	# [todo] put their times split since the last update
+	for track in diff:
+		models.Track.create(
+			sc_id = track,
+			target = target
+		)
+
+
 if __name__ == '__main__':
 	while True:
-
-		#collect some stuff
-		print("\nCollecting new data...\n")
 		
 		models.database.connect()
-		targets=models.Target.select()
+		targets = models.Target.select()
 		for target in targets:
-			print(target.permalink)
-
-			#Get a list of the previous favorites (from database)
-			prev_favorites = target.get_tracks()
-
-			print("There are {} previous favorites".format(prev_favorites.count()))
-
-			#Get a list of the current favorites (from Soundcloud)
-			favorites = sch.get_favorites(target.sc_id, 5)
-			
-			# If there are no previous, save the current ones!
-			if prev_favorites.count() == 0:
-				for track in favorites:
-					print("Adding {} to db".format(track))
-					models.Track.create(
-						sc_id = track,
-						target = target
-					)
-
-			for track in favorites:
-				print('  '+str(track))
+			update_target_in_db(target)
+			target.update_time()
+			target.save()
 
 		models.database.close()
 
-
 		#wait some time
-		time.sleep(10)
+		time.sleep(20)
